2e70011fc34c22e333a4597814f7718053bd6d3e
Alex <epicpiano8@gmail.com>
2013-04-23 17:58:18 -0300

    Began multitasking system

--- system/kernel.asm
+++ system/kernel.asm
@@ -2,15 +2,37 @@
 ;Developed by Alexander Kitaev.
 ;Copyleft Alexander Kitaev.
 
-;Write a string to the screen. Default color is blue on dark-green- please don't criticize my color choices!
+ORG [0x00008000]
+
+;IO Functions
 kernel_write_string:
-    mov ax,0x29; default colors
-    call write_string; this is actually the color output function- we call it with default colors
+    mov ax,0x29; default colors- blue on dark green. Please, don't criticize!
+    call write_string
     ret
 
 kernel_write_color_string:
     call write_string; this is actually the color output function
     ret
 
+kernel_input_string:
+    call input_string
+    ret
+
+;Execute a process (.proc file)
+kernel_execute_process:
+    call exec_process; it will return when a "wait" command is executed
+    ret
+
+;Multiprocessing functions- auto-executed by the kernel
+kernel_multiproc_watch:
+    ;need some way of making a process queue!
+    call kernel_execute_process
+    ;move to next process in queue
+    jmp kernel_multiproc_watch
+
+kernel_add_process:
+    ;add it to queue
+
 %include "output.asm"
 %include "input.asm"
+%include "procexec.asm"; to be added
