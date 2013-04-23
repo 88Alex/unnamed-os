c341a66d3cb3579f664271cd728d0f3a07af81c7
Alex <epicpiano8@gmail.com>
2013-04-23 17:28:53 -0300

    Update output.asm

--- system/output.asm
+++ system/output.asm
@@ -1,6 +1,11 @@
 write_string:
-    ;all the registers have been transferred to the stack except for ax
+    push bx
+    push cx
+    push dx
     mov bx,ax; the color's now in bx
+    pop dx
+    pop cx
+    pop bx
 write_string_loop:
     lodsb
     cmp ax,0
@@ -16,4 +21,6 @@ write_char:
     ret
 write_string_done:
     ret
+;=====================================
+;Variable declarations
 ScreenAddress db 0xb8000
