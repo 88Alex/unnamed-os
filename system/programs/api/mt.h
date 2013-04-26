bfb3f7979a86c24dcf8755b56074d73d363d0deb
Alex <epicpiano8@gmail.com>
2013-04-26 16:33:53 -0300

    Create mt.h

---
+++ system/programs/api/mt.h
@@ -0,0 +1,11 @@
+/*
+Multi-threading functions for Lithium-OS.
+Developed by Alexander Kitaev
+COPYLEFT Alexander Kitaev
+*/
+
+//NOTE: The system takes care of creating processes.
+
+void terminate();
+
+void wait();
