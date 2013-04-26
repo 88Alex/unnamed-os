9a832af31f3109fae2671900005169077b4335eb
Alex <epicpiano8@gmail.com>
2013-04-25 21:57:58 -0300

    Added support for newlines and unprintable chars

--- system/output.asm
+++ system/output.asm
@@ -3,24 +3,38 @@ write_string:
     push cx
     push dx
     mov bx,ax; the color's now in bx
+    call write_string_loop
     pop dx
     pop cx
     pop bx
+    ret
 write_string_loop:
     lodsb
-    cmp ax,0
+    cmp al,0
     je write_string_done
     call write_char
     jmp write_string_loop
 write_char:
-    ;the character is in ax
-    mov byte[ScreenAddress],ax
-    inc ScreenAddress
-    mov byte[ScreenAddress],bx; the color
-    inc ScreenAddress
+    ;the character is in al
+    cmp al,10
+    je write_newline
+    cmp al,33
+    jb write_invalid_char
+    ret
+write_newline:
+    mov xPos,0
+    inc yPos
+    ;TODO- write code handling screen overflow
+    jmp write_string_loop
+write_invalid_char:
+    lea ax,invalidCharMsg
+    call write_string
     ret
 write_string_done:
     ret
 ;=====================================
 ;Variable declarations
 ScreenAddress db 0xb8000
+xPos db 0
+yPos db 0
+invalidCharMsg db "Error- character cannot be displayed.",0
