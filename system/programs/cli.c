61b3826a89700917d4456a61224a6874dd62df1e
Alex <epicpiano8@gmail.com>
2013-04-26 22:40:20 -0300

    Update cli.c

--- system/programs/cli.c
+++ system/programs/cli.c
@@ -5,10 +5,16 @@ COPYLEFT Alexander Kitaev
 */
 
 #include "api/ciolib.h"
+#include "api/mtlib.h"
+#include "api/fiolib.h"
 
 char[] helpMessage1 = "exit- exits program\nhelp- displays help\n";
 char[] helpMessage2 = "version- displays version\ninfo- displays system information\n";
 char[] helpMessage3 = "Anything else- loads a program with that name.";
+
+char[] infoMessage1 = "Lithium-OS v0.1.0\n";
+char[] infoMessage2 = "An operating system developed by Alexander Kitaev.\n";
+char[] infoMessage3 = "COPYLEFT Alexander Kitaev, 2013.\n";
 
 int main()
 {
@@ -23,6 +29,24 @@ int main()
             print(helpMessage1);
             print(helpMessage2);
             print(helpMessage3);
+        }
+        else if(compareTwoStrings(userInput, "info"))
+        {
+            print(infoMessage1);
+            print(infoMessage2);
+            print(infoMessage3);
+        }
+        else
+        {
+            //Now we know it's not a built-in command
+            if(!fileExists(userInput))
+            {
+                print("Error: no such command or program.\n");
+            }
+            else
+            {
+                //load file contents into memory, and execute them
+            }
         }
     }
     return 0;
