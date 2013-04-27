32deeeecb7592357293daeaaccaa04897ef2787c
Alex <epicpiano8@gmail.com>
2013-04-27 19:33:53 -0300

    Added file i/o and "panic" functions

--- system/kernel.asm
+++ system/kernel.asm
@@ -5,6 +5,7 @@
 ORG [0x00009000]
 
 ;IO Functions
+
 kernel_write_string:;0x00009000
     mov al,0x29; default colors- blue on dark green. Please, don't criticize!
     call write_string
@@ -37,25 +38,72 @@ kernel_input_string:;0x00009080
 
 align 32
 
+;File function
+
+kernel_create_file:;0x000090A0
+    call create_file
+    ret
+
+align 32
+
+kernel_read_file:;0x000090C0
+    call read_file
+    ret
+
+align 32
+
+kernel_write_file:;0x000090E0
+    call write_file
+    ret
+
+align 32
+
+kernel_append_file:;0x0000A000
+    call append_file
+    ret
+
+align 32
+
+kernel_delete_file:;0x0000A020
+    call delete_file
+    ret
+
+align 32
+
+kernel_file_exists:;0x0000A040
+    call file_exists
+    ret
+
+align 32
+
 ;Multithreading functions
-kernel_add_proc:;0x000090A0
+
+kernel_add_proc:;0x0000A060
     call add_proc
     ret
 
 align 32
 
-kernel_proc_wait:;0x000090C0
+kernel_proc_wait:;0x0000A080
     call proc_wait
     ret
 
 align 32
 
-kernel_end_proc:;0x000090E0
+kernel_end_proc:;0x0000A0A0
     call end_proc
     ret
 
 align 32
 
+kernel_panic:;0x0000A0C0
+    lea si,panic_msg
+    mov al,0x0a;red on black
+    call kernel_write_color_string
+    ;shutdown
+
 %include "output.asm"
 %include "input.asm"
 %include "mt.asm";to be added
+
+panic_msg db "A fatal error occured. Lithium-OS is shutting down.",10,0
