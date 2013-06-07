23e641871ec2dc35c7dfbf878c0ce610e638c24e
Alex <epicpiano8@gmail.com>
2013-06-07 18:48:53 -0300

    Password-input function

--- system/input.asm
+++ system/input.asm
@@ -1,9 +1,33 @@
 input_str:
+    push eax
+    push ebx
+    push ecx
+    push edx
     call get_char_input; it's in kbddriver.asm
-    cmp ax,10
+    cmp al,10
     je input_str_done
-    lodsb
+    mov bl,al
+    stosb
+    ; Output that char
     jmp input_str
+input_password:
+    push eax
+    push ebx
+    push ecx
+    push edx
+    call get_char_input;
+    cmp al,10
+    je input_str_done
+    stosb
+    lea si,asterisk
+    call 0x00009000 ; Output string
 input_str_done:
-    ret; return to the kernel
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    ret
+
+asterisk db '*',0
+
 %include "kbddriver.asm"; to be added
